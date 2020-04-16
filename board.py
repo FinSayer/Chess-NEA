49116c335a9b538f86c62156ef6d7161d2774fd7
FinSayer <63777164+FinSayer@users.noreply.github.com>
2020-04-16 08:50:27 +0100

    Add files via upload

---
+++ board.py
@@ -0,0 +1,55 @@
+from pieces import *
+
+class Board:
+    def __init__(self):
+        self.rows, self.cols = (8, 8)
+        #creating a 2D array of size 8 by 8 filled with Empty classes
+        self.grid = [[Empty(0, [j, i]) for i in range(self.cols)] for j in range(self.rows)]
+
+    def start(self):
+        self.grid[0][0] = Rook(-1, [0, 0])
+        self.grid[0][1] = Knight(-1, [0, 1])
+        self.grid[0][2] = Bishop(-1, [0, 2])
+        self.grid[0][3] = Queen(-1, [0, 3])
+        self.grid[0][4] = King(-1, [0, 4])
+        self.grid[0][5] = Bishop(-1, [0, 5])
+        self.grid[0][6] = Knight(-1, [0, 6])
+        self.grid[0][7] = Rook(-1, [0, 7])
+
+        for i in range(self.cols):
+            self.grid[1][i] = Pawn(-1, [1, i])
+
+        self.grid[7][0] = Rook(1, [7, 0])
+        self.grid[7][1] = Knight(1, [7, 1])
+        self.grid[7][2] = Bishop(1, [7, 2])
+        self.grid[7][3] = Queen(1, [7, 3])
+        self.grid[7][4] = King(1, [7, 4])
+        self.grid[7][5] = Bishop(1, [7, 5])
+        self.grid[7][6] = Knight(1, [7, 6])
+        self.grid[7][7] = Rook(1, [7, 7])
+
+        for i in range(self.cols):
+            self.grid[6][i] = Pawn(1, [1, i])
+
+    def move(self):
+        x_pos = int(input("x >> "))
+        y_pos = int(input("y >> "))
+        self.grid[x_pos][y_pos].move()
+
+    def __str__(self):
+        for j in self.grid:
+            for e, i in enumerate(j):
+                if e == 7:
+                    print(str(i), end="\n")
+                else:
+                    print(str(i), end=" │ ")
+        return ""
+
+B = Board()
+
+B.start()
+
+
+while 1:
+    print(B)
+    B.move()
---
+++ pieces.py
@@ -0,0 +1,59 @@
+class Piece:
+    def __init__(self, alliance, position):
+        self.alliance = alliance
+        self.position = position
+
+class Empty(Piece):
+    def __str__(self):
+        return " "
+
+class Pawn(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        self.moveVector = [1, 0]
+
+    def move(self):
+        print("*moving*")
+
+    def __str__(self):
+        return "P" if self.alliance == -1 else "p"
+
+class Rook(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        pass
+
+    def __str__(self):
+        return "R" if self.alliance == -1 else "r"
+
+class Knight(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        pass
+
+    def __str__(self):
+        return "N" if self.alliance == -1 else "n"
+
+class Bishop(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        pass
+
+    def __str__(self):
+        return "B" if self.alliance == -1 else "b"
+
+class Queen(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        pass
+
+    def __str__(self):
+        return "Q" if self.alliance == -1 else "q"
+
+class King(Piece):
+    def __init__(self, alliance, position):
+        super().__init__(alliance, position)
+        pass
+
+    def __str__(self):
+        return "K" if self.alliance == -1 else "k"
